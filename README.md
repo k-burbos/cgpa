0f61cfb7cce2fdb1e554b8b2f6ec649644bc9594
unknown <kearaabigaelburbos@gmail.com>
2026-08-26 20:46:57 +0800

    added plan

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ AUGUST 24, 2026
 - added function makeNewFolder(); makes a new folder using the inputted string. 
 - added incomplete checkerHeader() function; currently does nothing. 
 - added function calcGWA(); calculates the GWA of the grades.csv file.
+
+AUGUST 26, 2026
+- renamed function makeNewFolder() into makeNewFile(); 
+- reformatted calcGWA(), displays up to 3 decimal points. 
+- added addYear() function; currently only displays text to show the function works. It does not actually make a folder.
--- main.cpp
+++ main.cpp
@@ -1,33 +1,77 @@
-#include <iostream>
 #include <fstream>
 #include <string>
+#include <iostream>
+
 using namespace std; 
 
 void addSubjects(); 
 void checkHeader(); // This function checks if it should add the header "Subject Code, Grade, Unit" or not.
-void makeFolder(string newFolderName); 
+void makeFile(string newFileName); 
 double calcGWA(); // This function just calculates the GWA.
+void addGWA();
+
+void addYear();
 
 int main(int argc, char** argv) {
-	int answer; 
+	int mainOp; 
 	do {
-		cout << "What do you want to do?\n1. Add Subjects\n2. Make New Grade Folder\n3. Exit\nAnswer: "; 
-		cin >> answer; 
-		switch(answer) {
+		cout << "1. Add (Year/Sem/Grades)\n";
+		cout << "2. Edit Grades\n";
+		cout << "3. Display Grades\n";
+		cout << "4. Remove (Year/Sem/Grades)\n";
+		cout << "5. Exit\n";
+		cout << "Choose an option: ";
+		cin >> mainOp; 
+		switch(mainOp) {
 			case 1: 
-				addSubjects();
+				int firstOp;
+				std::cout << "1. Add Year\n";
+				std::cout << "2. Add Semester\n";
+				std::cout << "3. Add Grades\n";
+				std::cout << "4. Exit\n";
+				std::cout << "Choose an option: ";
+				std::cin >> firstOp;
+				switch(firstOp){
+					case 1: 
+						addYear(); 
+						break;  
+					case 4:
+						return 0;
+				}
 				break;
+//				addSubjects();
+//				break;
 			case 2: 
-				string newFolderName; 
-				cout << "What is the name of this grade folder? "; 
-				cin >> newFolderName; 
-				newFolderName += ".csv"; 
-				makeFolder(newFolderName); 
-				break;
+			break;
+//				string newFileName; 
+//				cout << "What is the name of this grade folder? (Please, do not use spaces): "; 
+//				cin >> newFileName; 
+//				newFileName += ".csv"; 
+//				makeFile(newFileName); 
+//				break;
 		}
-	} while(answer != 3); 
+	} while(mainOp != 3); 
 	calcGWA();
 	return 0;
+}
+
+void addYear() {
+	cout << "Input year: ";  
+	int year; 
+	cin >> year; 
+	cout << "Added " << year; 
+	if(year % 10 == 1) {
+		cout << "st "; 
+	} else if (year % 10 == 2) {
+		cout << "nd "; 
+	} else if (year % 10 == 3) {
+		cout << "rd "; 
+	} else {
+		cout << "th ";
+	}
+	
+	cout << "year folder.\n\n";
+	return; 
 }
 
 void addSubjects() {
@@ -60,11 +104,13 @@ void addSubjects() {
 	cout << "\n\nYou have added " << totalSubjects << " subjects!"; 
 }
 
-void makeFolder(string newFolderName) {
+void makeFile(string newFileName) {
 	ofstream file; 
-	file.open(newFolderName.c_str()); 
+	file.open(newFileName.c_str()); 
 	if(file.is_open()) {
-		cout << "Folder has been made succesfully!"; 
+		cout << "\nFile has been made succesfully!\n\n"; 
+		file.close();
+		return;
 	}
 }
 
@@ -74,21 +120,21 @@ double calcGWA(){
 	int location = 0;
 	double ret = 0, unitTotal = 0;
 	
-	f.open("grades.csv", ios::in);
-	while(getline(f,l)){
-		location = l.find(',');
-		string tmp = l.substr(0,location);
-		l = l.substr(location + 1, l.length());
-		location = l.find(',');
-		double grade = std::stod(l.substr(0,location));
-		l = l.substr(location + 1, l.length());
-		double unit = std::stod(l);
-		ret += grade * unit;
-		unitTotal += unit;
-		std::cout << tmp << ": " << grade << ", " << unit << endl;
-	}
-	
-	std::cout << ret / unitTotal; // debugging purposes
+//	f.open("grades.csv", ios::in);
+//	while(getline(f,l)){
+//		location = l.find(',');
+//		string tmp = l.substr(0,location);
+//		l = l.substr(location + 1, l.length());
+//		location = l.find(',');
+//		double grade = std::stod(l.substr(0,location));
+//		l = l.substr(location + 1, l.length());
+//		double unit = std::stod(l);
+//		ret += grade * unit;
+//		unitTotal += unit;
+////		std::cout << tmp << ": " << grade << ", " << unit << endl;
+//	}
+//	
+//	std::printf("GWA: %.3f", ret / unitTotal); // debugging purposes
 	return ret / unitTotal;
 }
 
